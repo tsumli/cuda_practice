bcc77246eea94e2fdd9989eb3fa93c8e05bd0b88
hatakeyama <28624172+tsumli@users.noreply.github.com>
2024-08-05 22:40:35 +0900

    add 04-example

---
+++ src/04-cooperative-groups/main.cu
@@ -0,0 +1,40 @@
+/**
+ * @file main.cu
+ * @brief https://developer.nvidia.com/blog/cooperative-groups/
+ */
+
+#include <cooperative_groups.h>
+#include <cuda.h>
+#include <cuda_runtime.h>
+
+#include "common/exception.h"
+
+namespace cg = cooperative_groups;
+
+__global__ void my_kernel() {
+    cg::thread_block block = cg::this_thread_block();
+
+    dim3 dim_threads = block.dim_threads();
+    uint num_threads = block.num_threads();
+    uint get_type = block.get_type();
+    dim3 group_dim = block.group_dim();
+    dim3 group_index = block.group_index();
+    dim3 thread_index = block.thread_index();
+    uint size = block.size();
+    uint thread_rank = block.thread_rank();
+
+    printf("dim_threads: (%d, %d, %d)\n", dim_threads.x, dim_threads.y, dim_threads.z);
+    // printf("num_threads: %d\n", num_threads);
+    // printf("get_type: %d\n", get_type);
+    // printf("group_dim: (%d, %d, %d)\n", group_dim.x, group_dim.y, group_dim.z);
+    // printf("group_index: (%d, %d, %d)\n", group_index.x, group_index.y, group_index.z);
+    // printf("thread_index: (%d, %d, %d)\n", thread_index.x, thread_index.y, thread_index.z);
+    // printf("size: %d\n", size);
+    // printf("thread_rank: %d\n", thread_rank);
+}
+
+int main() {
+    my_kernel<<<dim3{2, 3, 4}, dim3{5, 6, 7}>>>();
+    THROW_IF_FAILED(cudaDeviceSynchronize());
+    return 0;
+}
--- src/common/cuda/utils.h
+++ src/common/cuda/utils.h
@@ -27,6 +27,11 @@ template <typename T>
 void CopyToDevice(T* value_device, const T* const value_host, const std::size_t size) {
     THROW_IF_FAILED(cudaMemcpy(value_device, value_host, sizeof(T) * size, cudaMemcpyHostToDevice));
 }
+
+template <typename T>
+constexpr T DivUp(T value, T s) {
+    return (value + s - 1) / s;
+}
 }  // namespace cupr::cuda
 
 #endif  // SRC_COMMON_CUDA_UTILS_H_
